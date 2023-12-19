01cf325ed98fcb91008ac87b1287ac6495742b78
unknown <znaxor74@mail.ru>
2023-12-19 12:31:43 +0300

    fix: done carbon

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,17 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  const k = 0.693/HALF_LIFE_PERIOD;
+  if(typeof sampleActivity === "string"){
+    let input = Number(sampleActivity);
+    if(0 < input && Number.isFinite(input)){
+      let t = Math.ceil(Math.log(MODERN_ACTIVITY/sampleActivity)/k);
+      if(t > 0){
+        return t;
+      } return false;
+    }return false;
+  }return false;
 }
 
 module.exports = {
