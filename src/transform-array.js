525359444ca3d54bc3500f5e8cee152726546752
Леха <znaxor74@mail.ru>
2023-12-20 01:30:08 +0300

    fix: start the tranform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,11 +13,92 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if(!Array.isArray(arr)){
+    throw new Error(`'arr' parameter must be an instance of the Array!`);
+  }else if(arr.length === 0){
+    return arr;
+  }else{
+    let newArr = [];
+    newArr = arr.slice(0);
+    for(let i = 0; i < newArr.length; i++){
+      if(newArr[i] === '--double-next'){
+        if(newArr[newArr.length - 1] === '--double-next'){
+          newArr.pop();
+          return newArr;
+        }else{
+          newArr[i] = newArr[i+1];
+          return newArr;
+        }
+
+      }else if(newArr[i] === '--discard-prev'){
+        if(newArr[i-1] === undefined){
+          newArr.shift();
+          return newArr;
+        }else{
+          newArr.splice(newArr[i-1],2);
+          return newArr;
+        }
+      }else if (newArr[i] === '--discard-next'){
+        if(newArr[newArr.length - 1] === '--discard-next'){
+          newArr.pop();
+          return newArr;
+        }else{       
+          newArr.splice(newArr[i],2);
+          return newArr;
+        }
+      }else if(newArr[i] === '--double-prev'){
+        if(newArr[i-1] === undefined){
+          newArr = newArr.slice(0);
+          newArr.shift();
+          return newArr;
+        }else{
+          newArr[i] = newArr[i-1];
+          return newArr;
+        }
+        
+      }
+    }
+  }return arr;
 }
 
 module.exports = {
   transform
 };
+
+
+
+// function transform(arr) {
+//   let newArr = [];
+//   if(!Array.isArray(arr)){
+//     throw new Error(`'arr' parameter must be an instance of the Array!`);
+//   }else if(arr.length === 0){
+//     return arr;
+//   }else{
+//     for(let i = 0; i < arr.length; i++){
+//       if(arr[i] === '--double-next'){
+//         arr[i] = arr[i+1];
+//         return arr;
+//       }else if(arr[i] === '--discard-prev'){
+//         if(arr[i-1] === undefined){
+//           newArr = arr.slice(0);
+//           newArr.shift();
+//           return newArr;
+//         }else{
+//           arr.splice(arr[i-1],2);
+//           return arr;
+//         }
+//       }else if (arr[i] === '--discard-next'){
+//         arr.splice(arr[i],2);
+//         return arr;
+//       }else if(arr[i] === '--double-prev'){
+//         arr[i] = arr[i-1];
+//         return arr;
+//       }
+//     }
+//   }return arr;
+// }
+
+// module.exports = {
+//   transform
+// };
--- test.js
+++ test.js
@@ -1,15 +1,54 @@
-function calculateHanoi(disksNumber, turnsSpeed) {
-  let turns = (2^disksNumber) - 1;
-  let seconds= Math.floor((turns/turnsSpeed) * 3600)
-  console.log(2^disksNumber)
-  console.log({
-    turns,
-    seconds
-  })
-  return {
-    turns,
-    seconds
-  }
+function transform(arr) {
+  if(!Array.isArray(arr)){
+    throw new Error(`'arr' parameter must be an instance of the Array!`);
+  }else if(arr.length === 0){
+    return arr;
+  }else{
+    let newArr = [];
+    newArr = arr.slice(0);
+    for(let i = 0; i < newArr.length; i++){
+      if(newArr[i] === '--double-next'){
+        console.log(newArr[newArr.length - 1])
+        if(newArr.length - 1 === '--double-next'){
+          newArr.pop();
+          console.log(newArr)
+          return newArr;
+        }else{
+          newArr[i] = newArr[i+1];
+          return newArr;
+        }
+
+      }else if(newArr[i] === '--discard-prev'){
+        if(newArr[i-1] === undefined){
+          newArr.shift();
+          return newArr;
+        }else{
+          newArr.splice(newArr[i-1],2);
+          return newArr;
+        }
+      }else if (newArr[i] === '--discard-next'){
+        console.log(newArr.length -1)
+        if(newArr.length - 1 === '--discard-next'){
+          newArr.pop();
+          console.log(newArr)
+          return newArr;
+        }else{       
+          newArr.splice(newArr[i],2);
+          return newArr;
+        }
+      }else if(newArr[i] === '--double-prev'){
+        if(newArr[i-1] === undefined){
+          newArr = newArr.slice(0);
+          newArr.shift();
+          return newArr;
+        }else{
+          newArr[i] = newArr[i-1];
+          return newArr;
+        }
+        
+      }
+    }
+  }return arr;
 }
 
-calculateHanoi(5, 4074)
+transform( [1, 2, 3, '--double-next']);
