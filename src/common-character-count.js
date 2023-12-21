40fef5acff64bd65fbfe076d7c0ae772e4c57190
unknown <znaxor74@mail.ru>
2023-12-21 13:35:18 +0300

    fix: common character

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,11 +11,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function getCommonCharacterCount(s1, s2) {
+  let firstArr = s1.split('');
+  let secondArr = s2.split('');
+  let count = 0;
+  if (firstArr.length === 0 || secondArr.length === 0) {
+    return 0;
+  } else {
+    for (let i = 0; i < firstArr.length; i++) {
+      let current = secondArr.indexOf(firstArr[i]);
+      if (current !== -1) {
+        secondArr.splice(current, 1);
+        count++;
+      }
+    }
+    return count;
+  }
 }
-
 module.exports = {
   getCommonCharacterCount
 };
--- test.js
+++ test.js
@@ -1,54 +1,76 @@
-function transform(arr) {
-  if(!Array.isArray(arr)){
-    throw new Error(`'arr' parameter must be an instance of the Array!`);
-  }else if(arr.length === 0){
-    return arr;
-  }else{
-    let newArr = [];
-    newArr = arr.slice(0);
-    for(let i = 0; i < newArr.length; i++){
-      if(newArr[i] === '--double-next'){
-        if(newArr[newArr.length - 1] === '--double-next'){
-          newArr.pop();
-          return newArr;
-        }else{
-          newArr[i] = newArr[i+1];
-          return newArr;
-        }
+// function transform(arr) {
+//   if(!Array.isArray(arr)){
+//     throw new Error(`'arr' parameter must be an instance of the Array!`);
+//   }else if(arr.length === 0){
+//     return arr;
+//   }else{
+//     let newArr = [];
+//     newArr = arr.slice(0);
+//     for(let i = 0; i < newArr.length; i++){
+//       if(newArr[i] === '--double-next'){
+//         if(newArr[newArr.length - 1] === '--double-next'){
+//           newArr.pop();
+//           return newArr;
+//         }else{
+//           newArr[i] = newArr[i+1];
+//           return newArr;
+//         }
 
-      }else if(newArr[i] === '--discard-prev'){
-        if(newArr[i-1] === undefined){
-          newArr.shift();
-          return newArr;
-        }else{
-          newArr.splice(newArr[i-1],2);
-          return newArr;
-        }
-      }else if(newArr[i] === '--discard-next' && newArr[i+2] === '--double-prev' || newArr[i] === '--discard-next' && newArr[i+2] === '--discard-prev'){
-        newArr.splice(i,3);
-        return newArr;
-      }else if (newArr[i] === '--discard-next'){
-        if(newArr[newArr.length - 1] === '--discard-next'){
-          newArr.pop();
-          return newArr;
-        }else{       
-          newArr.splice(i,2);
-          return newArr;
-        }
-      }else if(newArr[i] === '--double-prev'){
-        if(newArr[i-1] === undefined){
-          newArr = newArr.slice(0);
-          newArr.shift();
-          return newArr;
-        }else{
-          newArr[i] = newArr[i-1];
-          return newArr;
-        }
+//       }else if(newArr[i] === '--discard-prev'){
+//         if(newArr[i-1] === undefined){
+//           newArr.shift();
+//           return newArr;
+//         }else{
+//           newArr.splice(newArr[i-1],2);
+//           return newArr;
+//         }
+//       }else if(newArr[i] === '--discard-next' && newArr[i+2] === '--double-prev' || newArr[i] === '--discard-next' && newArr[i+2] === '--discard-prev'){
+//         newArr.splice(i,3);
+//         return newArr;
+//       }else if (newArr[i] === '--discard-next'){
+//         if(newArr[newArr.length - 1] === '--discard-next'){
+//           newArr.pop();
+//           return newArr;
+//         }else{       
+//           newArr.splice(i,2);
+//           return newArr;
+//         }
+//       }else if(newArr[i] === '--double-prev'){
+//         if(newArr[i-1] === undefined){
+//           newArr = newArr.slice(0);
+//           newArr.shift();
+//           return newArr;
+//         }else{
+//           newArr[i] = newArr[i-1];
+//           return newArr;
+//         }
         
+//       }
+//     }
+//   }return arr;
+// }
+
+// // transform(  [1, 2, 3, '--discard-next', 1337, '--double-prev', 4, 5]);
+// console.log(transform(  [ 3, '--double-next', '1']))
+
+function getCommonCharacterCount(s1, s2) {
+  let firstArr = s1.split('');
+  let secondArr = s2.split('');
+  let count = 0;
+  if (firstArr.length === 0 || secondArr.length === 0) {
+    return 0;
+  } else {
+    for (let i = 0; i < firstArr.length; i++) {
+      let index = secondArr.indexOf(firstArr[i]);
+      if (index !== -1) {
+        secondArr.splice(index, 1);
+        count++;
       }
     }
-  }return arr;
+    return count;
+  }
 }
+console.log(getCommonCharacterCount('aabcc', 'adcaa'));
 
-// transform(  [1, 2, 3, '--discard-next', 1337, '--double-prev', 4, 5]);
-console.log(transform(  [ 3, '--double-next', '1']))
+
+console.log(getCommonCharacterCount('aabcc', 'adcaa'));
