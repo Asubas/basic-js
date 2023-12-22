20f2ec3d7feb141e807153a79ac723e9914e0192
Леха <znaxor74@mail.ru>
2023-12-23 01:26:39 +0300

    fix:done file-names

--- src/file-names.js
+++ src/file-names.js
@@ -15,9 +15,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+  for (let index = 0; index < names.length; index++) {
+    const elem = names[index];
+    let count = 1;
+    for (let j = index + 1; j < names.length; j++) {
+      if (names[j] === elem) {
+        const newName = `${elem}(${count})`;
+        names[j] = newName;
+        count++;
+      }
+    }
+  }
+  
+  return names;
 }
 
 module.exports = {
--- test.js
+++ test.js
@@ -1,7 +1,31 @@
-function getEmailDomain(email) {
-  const arr = email.split('@');
-  arr.reverse();
-  return arr[0].toString();
+// function renameFiles(names) {
+//   let i = 1;
+//   names.forEach((elem,index) => {
+//     if(elem === names[index + 1]){
+//       names[index + 1] += `(${i})`;
+//       i++
+//     } else if(elem === `names[index + 1](${1})`){
+//       names[index + 1] += '(1)'
+//     }
+//   });
+//   return names;
+// }
+
+function renameFiles(names) {
+  for (let index = 0; index < names.length; index++) {
+    const elem = names[index];
+    let count = 1;
+    for (let j = index + 1; j < names.length; j++) {
+      if (names[j] === elem) {
+        const newName = `${elem}(${count})`;
+        names[j] = newName;
+        count++;
+      }
+    }
+  }
+  
+  return names;
 }
 
-console.log(getEmailDomain('prettyandsimple@example.com'))
+
+console.log(renameFiles(["file", "file", "image", "file(1)", "file"]))
